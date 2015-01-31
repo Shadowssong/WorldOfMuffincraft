fe8752fb35f59da1df90b8617b97ad334b827a71
Shadowssong <shadowssong@gmail.com>
2015-01-31 10:40:31 -0500

    started guild page

---
+++ app/controllers/guilds_controller.rb
@@ -0,0 +1,10 @@
+class GuildsController < ApplicationController
+  def index
+    #
+  end
+
+  def show
+    @nav_elements = [ "Summary", "Roster", "News", "Achievements", "Challenge Mode" ]
+    #
+  end
+end
