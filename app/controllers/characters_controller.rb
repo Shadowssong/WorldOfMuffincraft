3b6f35144b335b0404a82dda065f0022eea1c926
Shadowssong <shadowssong@gmail.com>
2015-02-01 19:45:40 -0500

    added achievement page

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,8 @@
 require 'battle-muffin'
+require 'date'
 
 class CharactersController < ApplicationController
-  helper_method :build_stats_string, :get_race, :get_background, :get_profile_main, :get_guild, :get_item_url, :name,  :average_item_level, :average_item_level_equipped, :achievement_points, :get_item_id, :get_icon_url
+  helper_method :build_stats_string, :get_race, :get_background, :get_profile_main, :get_guild, :get_item_url, :name,  :average_item_level, :average_item_level_equipped, :achievement_points, :get_item_id, :get_icon_url, :achievement_array
 
   def index
     #
@@ -15,15 +16,12 @@ class CharactersController < ApplicationController
     @all_info           = @character.all_info
     @items              = @all_info['items']
     @title              = get_current_title(@all_info['titles'])
-
-    @nav_bar            = ["Summary", "Hunter Pets", "Auctions", "Events", "Achievements", "Challenge Mode", "Pets & Mounts", "Professions", "Reputation", "PvP", "Activity", "Feed", "Guild"]
     @left_column_gear   = ["head", "neck", "shoulder", "back", "chest", "shirt", "tabard", "wrist" ]
     @right_column_gear  = ["hands", "waist", "legs", "feet", "finger1", "finger2", "trinket1", "trinket2" ]
     @mainhand           = [ "mainHand", "offHand" ] 
   end
 
   def challenge_mode
-    #
     @client             = BattleMuffin.new("7argtwb4rtuy2ccwcfjs74eapm52juhv") 
     char_name          = params[:id].split("_").first.strip
     @realm              = params[:id].split("_").last.strip
@@ -31,12 +29,9 @@ class CharactersController < ApplicationController
     @all_info           = @character.all_info
     @items              = @all_info['items']
     @title              = get_current_title(@all_info['titles'])
-
-    @nav_bar            = ["Summary", "Hunter Pets", "Auctions", "Events", "Achievements", "Challenge Mode", "Pets & Mounts", "Professions", "Reputation", "PvP", "Activity", "Feed", "Guild"]
   end
 
   def pets_mounts
-    #
     @client             = BattleMuffin.new("7argtwb4rtuy2ccwcfjs74eapm52juhv") 
     char_name          = params[:id].split("_").first.strip
     @realm              = params[:id].split("_").last.strip
@@ -44,10 +39,17 @@ class CharactersController < ApplicationController
     @all_info           = @character.all_info
     @items              = @all_info['items']
     @title              = get_current_title(@all_info['titles'])
-
-    @nav_bar            = ["Summary", "Hunter Pets", "Auctions", "Events", "Achievements", "Challenge Mode", "Pets & Mounts", "Professions", "Reputation", "PvP", "Activity", "Feed", "Guild"]
   end
 
+  def achievements
+    @client             = BattleMuffin.new("7argtwb4rtuy2ccwcfjs74eapm52juhv") 
+    char_name          = params[:id].split("_").first.strip
+    @realm              = params[:id].split("_").last.strip
+    @character          = @client.character_handler.search(@realm, char_name) 
+    @all_info           = @character.all_info
+    @items              = @all_info['items']
+    @title              = get_current_title(@all_info['titles'])
+  end
 
   def get_current_title(titles)
     titles.each do |title|
@@ -133,5 +135,32 @@ class CharactersController < ApplicationController
 
   def achievement_points
     @all_info['achievementPoints']
+  end
+
+  def achievement_array
+    achieves = [ ]
+    @all_info['achievements']['achievementsCompleted'].reverse.first(25).each_with_index do |ach, index|
+      tmp = { }
+      tmp[:id] = ach
+      tmp[:time] = get_achieve_timestamp(index)
+      tmp[:name] = get_achievement(ach)
+      tmp[:timestamp] = get_achieve_timestamp(index)
+      achieves << tmp
+    end
+    achieves.sort_by { |hsh| hsh[:time] }.reverse
+  end
+
+  def get_achievement(ach)
+    get_achievement_name(ach)
+  end
+
+
+  def get_achieve_timestamp(index)
+    timestamp = @all_info['achievements']['achievementsCompletedTimestamp'][index]
+    DateTime.strptime(timestamp.to_s,'%Q').strftime("%c")
+  end
+
+  def get_achievement_name(id)
+    Nokogiri::HTML(HTTParty.get("http://www.wowhead.com/achievement=#{id}")).title.split("- Achievement").first.strip
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/characters/challenge_mode/:id', to: 'characters#challenge_mode'
   get '/characters/pets_mounts/:id', to: 'characters#pets_mounts'
+  get '/characters/achievements/:id', to: 'characters#achievements'
 
   root to: 'visitors#index'
 end
