2519174fc669b4301029ce86dbd25265d1bf2efa
Shadowssong <shadowssong@gmail.com>
2015-02-02 00:01:33 -0500

    fixed links and sorted reputation differently

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   end
   resources :guilds
 
-  get '/characters/challenge_mode/:id', to: 'characters#challenge_mode'
-  get '/characters/pets_mounts/:id', to: 'characters#pets_mounts'
-  get '/characters/achievements/:id', to: 'characters#achievements'
-  get '/characters/progression/:id', to: 'characters#progression'
-  get '/characters/reputation/:id', to: 'characters#reputation'
+  get '/characters/challenge_mode/:id', to: 'characters#challenge_mode', as: :challenge_mode
+  get '/characters/pets_mounts/:id', to: 'characters#pets_mounts', as: :pets_mounts
+  get '/characters/achievements/:id', to: 'characters#achievements', as: :achievements
+  get '/characters/progression/:id', to: 'characters#progression', as: :progression
+  get '/characters/reputation/:id', to: 'characters#reputation', as: :reputation
 
   root to: 'visitors#index'
 end
